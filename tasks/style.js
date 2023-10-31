fedec6c3211737bab9635157346840955a7b6b17
Den <den204d@gmail.com>
2023-10-31 20:49:18 +0200

    clean gulp build

--- tasks/style.js
+++ tasks/style.js
@@ -3,7 +3,6 @@ const {
   dest
 } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-// const bulk = require('gulp-sass-bulk-importer');
 const prefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean-css');
 const concat = require('gulp-concat');
@@ -13,7 +12,6 @@ const bs = require('browser-sync');
 module.exports = function style() {
   return src('src/scss/**/*.scss')
     .pipe(map.init())
-    // .pipe(bulk())
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
